cb889f8f5cb352537edeac20d1a536e14c0c2ac0
Julia Moś <juliamos45@gmail.com>
2022-05-28 08:50:17 +0200

    Added model for register and entity for roles

---
+++ AuthService/Entities/RoleEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace AuthService.Entities
+{
+    public class RoleEntity
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
---
+++ AuthService/Models/RegisterModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthService.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "First name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
